a74d746d7e28525ff6b3c60ac29e91736b9b190e
eller <martin@ellermeier.net>
2017-03-14 21:18:47 +0100

    Added WB Actions with payload.

--- src/app/whiteboard/whiteboard.actions.ts
+++ src/app/whiteboard/whiteboard.actions.ts
@@ -3,18 +3,18 @@
  */
 
 import {Injectable} from "@angular/core";
-import { Action } from 'redux';
+import {MyAction} from "../../store";
 
 @Injectable()
 export class WhiteboardActions {
   static WB_INCREASE_WIDTH = "WB_INCREASE_WIDTH";
   static WB_INCREASE_HEIGTH = "wB_INCREASE_HEIGHT";
 
-  static increaseWidth(): Action {
-    return {type: this.WB_INCREASE_HEIGTH};
+  static increaseWidth(width: number): MyAction {
+    return {type: this.WB_INCREASE_WIDTH, payload: width};
   }
 
-  static increaseHeight(): Action {
-    return {type: this.WB_INCREASE_HEIGTH};
+  static increaseHeight(height: number): MyAction {
+    return {type: this.WB_INCREASE_HEIGTH, payload: height};
   }
 }
--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,13 @@
-import { Action } from 'redux';
 import {CounterActions} from "./app/app.actions";
 import {WhiteboardActions} from "./app/whiteboard/whiteboard.actions";
 /**
  * Created by eller on 09.03.17.
  */
+
+export interface MyAction {
+  type: string,
+  payload: any,
+}
 
 export interface IAppState {
   count: number;
@@ -23,16 +27,18 @@ export const INITIAL_STATE: IAppState = {
   // End of Whiteboard
 };
 
-export function rootReducer(lastState: IAppState, action: Action) {
+export function rootReducer(lastState: IAppState, action: MyAction) {
   switch(action.type) {
     case CounterActions.INCREMENT:
       return Object.assign({}, lastState, {count: lastState.count + 1});
     case CounterActions.DECREMENT:
       if( lastState.count > 0)
         return Object.assign({}, lastState, {count: lastState.count - 1});
+      break;
+    case WhiteboardActions.WB_INCREASE_WIDTH:
+      return Object.assign({}, lastState, {wbWidth: lastState.wbWidth+ action.payload});
     case WhiteboardActions.WB_INCREASE_HEIGTH:
-      return Object.assign({}, lastState, {wbWidth: lastState.wbHeight + 30});
-              // ToDo work with payload!
+      return Object.assign({}, lastState, {wbHeight: lastState.wbHeight + action.payload});
 
   }
   // Always return the current state for unkown actions!
